f4d405f91b8e879fcb91934829827ccdcbbc00ab
Sóstenes Apollo <sostenesapollo25@gmail.com>
2022-06-17 21:42:00 -0300

    chore: modules

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+
+}
+
+provider "aws" {
+
+}
+
+resource "aws_instance" "vm1" {
+
+}
+
+data "aws_ami" "ami" {
+
+}
+
+module "vpc" {
+
+}
+
+variable "ip" {
+
+}
+
+output "vm1_ip" {
+
+}
+
+locals {
+
+}
+
