16a68184c49471fb7039a86d3ecb16f9c8807cc2
Sóstenes Apollo <sostenesapollo25@gmail.com>
2022-06-19 15:12:42 -0300

    feat: creating s3 bucket

--- main.tf
+++ main.tf
@@ -1,32 +1,14 @@
-terraform {
-
-}
-
 provider "aws" {
-
+  region  = "us-east-1"
+  profile = "tf01"
 }
 
-resource "aws_instance" "vm1" {
+resource "aws_s3_bucket" "my-test-bucket" {
+  bucket = "my-tf-test-bucket-19283718937128932"
 
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    Managedby   = "Terraform"
+  }
 }
-
-data "aws_ami" "ami" {
-
-}
-
-module "vpc" {
-
-}
-
-variable "ip" {
-
-}
-
-output "vm1_ip" {
-
-}
-
-locals {
-
-}
-
