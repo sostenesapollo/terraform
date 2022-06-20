0533349cf457e23c6d9b0a7d1202999169542c7c
Sóstenes Apollo <sostenesapollo25@gmail.com>
2022-06-20 08:04:14 -0300

    feat: versions

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = "1.2.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.19.0"
+    }
+  }
+}
+
 provider "aws" {
   region  = "us-east-1"
   profile = "tf01"
