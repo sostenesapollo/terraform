d016f87f4eb6633ac7ddd77f2061ec90aac7df18
Sóstenes Apollo <sostenesapollo25@gmail.com>
2022-06-26 10:35:54 -0300

    feat: host static website in s3

--- intermediary/data-source/ec2/main.tf
+++ intermediary/data-source/ec2/main.tf
@@ -9,10 +9,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "tfstate-968339500772"
+    bucket  = "tfstate-968339500772123"
     key     = "dev/03-data-sources-s3/terraform.tfstate"
-    region  = "eu-central-1"
-    profile = "tf014"
+    region  = "us-east-1"
+    profile = "tf01"
   }
 }
 
